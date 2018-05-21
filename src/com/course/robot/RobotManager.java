c35c2ab1d968e846d49eeec98427aefa7449f564
Artsem Ilyayeu <artsemi@playtika.com>
2018-05-21 20:58:40 +0300

    massives commit

--- src/com/course/robot/RobotManager.java
+++ src/com/course/robot/RobotManager.java
@@ -5,20 +5,18 @@ import javax.swing.*;
 public class RobotManager {
     public static void main(String[] args) {
 
-        final int COUNT = 12;
-        final int SIDE = 600 / COUNT;
-        final int alfa = 360 / COUNT / 2;
-        Robot robot = new Robot(50, 100);
-        for (int i = 0; i < COUNT; i++) {
-            robot.forward(SIDE);
-            robot.setCourse(robot.getCourse() - 2 * alfa);
-            robot.forward(SIDE);
-            robot.setCourse(robot.getCourse() + 360 / (double) COUNT + 2 * alfa);
+        Robot[] rbts = new Robot[10];
+
+        // Обратите внимание на запись rbts.length - это свойство
+        // (неизменяемое) возращает размер массива
+        for (int i = 0; i < rbts.length; i++) {
+            // Создаем объект типа Robot
+            rbts[i] = new Robot(i * 10, i * 10);
         }
 
-        // Создаем форму для отрисовки пути нашего робота
-        RobotFrame rf = new RobotFrame(robot);
-        rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        rf.setVisible(true);
+        // Еще один цикл, который вызывает печать координат у каждого робота
+        for (int i = 0; i < rbts.length; i++) {
+            rbts[i].printCoordinates();
+        }
     }
 }
