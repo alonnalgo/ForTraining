b453d44f110d9a90313bba2222bc2a2fa1f1dff2
artsem.ilyayeu <artsem.ilyayeu@algosec.com>
2022-09-06 16:31:48 +0300

    commented all rules except 1

--- src/firewall-ingress.tf
+++ src/firewall-ingress.tf
@@ -1,4 +1,4 @@
-resource "google_compute_firewall" "i01-ni-gcp" {
+resource "google_compute_firewall" "default" {
   name = "devsecops-fw-all"
   network = "devsecops-network-gcp"
 
@@ -11,99 +11,99 @@ resource "google_compute_firewall" "i01-ni-gcp" {
     "0.0.0.0/0"]
 }
 
-resource "google_compute_firewall" "i04-ni-gcp" {
-  name = "devsecops-fw-single-port"
-  network = "devsecops-network-gcp"
-
-  allow {
-    protocol = "tcp"
-    ports = [
-      "389"]
-  }
-  allow {
-    protocol = "udp"
-    ports = [
-      "389"]
-  }
-
-  description = "INGRESS"
-  source_ranges = [
-    "0.0.0.0/0"]
-}
-
-
-resource "google_compute_firewall" "i05-06-ni-gcp" {
-  name = "devsecops-fw-multi-ports"
-  network = "devsecops-network-gcp"
-
-  allow {
-    protocol = "tcp"
-    ports = [
-      "3020-3050",
-      "9000"]
-  }
-  allow {
-    protocol = "udp"
-    ports = [
-      "3020-3050",
-      "9000"]
-  }
-
-  description = "INGRESS"
-  source_ranges = [
-    "0.0.0.0/0"]
-}
-
-
-resource "google_compute_firewall" "i14-ni-gcp" {
-  name = "devsecops-fw-single-ip"
-  network = "devsecops-network-gcp"
-
-  allow {
-    protocol = "tcp"
-    ports = [
-      "135",
-      "139",
-      "445",
-      "593"]
-  }
-
-  description = "INGRESS"
-  source_ranges = [
-    "192.168.1.1/32"]
-}
-
-resource "google_compute_firewall" "i23-ni-gcp" {
-  name = "devsecops-fw-multi-ip"
-  network = "devsecops-network-gcp"
-
-  allow {
-    protocol = "udp"
-    ports = [
-      "137",
-      "138"]
-  }
-
-  description = "INGRESS"
-  source_ranges = [
-    "192.168.1.1/32",
-    "192.168.1.2/32",
-    "192.168.1.3/32",
-    "192.168.1.4/32",
-    "192.168.1.5/32"]
-}
-
-resource "google_compute_firewall" "i21-ni-gcp" {
-  name = "devsecops-fw-cidr-ip"
-  network = "devsecops-network-gcp"
-
-  allow {
-    protocol = "tcp"
-    ports = [
-      "22"]
-  }
-
-  description = "INGRESS"
-  source_ranges = [
-    "192.168.0.0/16"]
-}
+//resource "google_compute_firewall" "i04-ni-gcp" {
+//  name = "devsecops-fw-single-port"
+//  network = "devsecops-network-gcp"
+//
+//  allow {
+//    protocol = "tcp"
+//    ports = [
+//      "389"]
+//  }
+//  allow {
+//    protocol = "udp"
+//    ports = [
+//      "389"]
+//  }
+//
+//  description = "INGRESS"
+//  source_ranges = [
+//    "0.0.0.0/0"]
+//}
+//
+//
+//resource "google_compute_firewall" "i05-06-ni-gcp" {
+//  name = "devsecops-fw-multi-ports"
+//  network = "devsecops-network-gcp"
+//
+//  allow {
+//    protocol = "tcp"
+//    ports = [
+//      "3020-3050",
+//      "9000"]
+//  }
+//  allow {
+//    protocol = "udp"
+//    ports = [
+//      "3020-3050",
+//      "9000"]
+//  }
+//
+//  description = "INGRESS"
+//  source_ranges = [
+//    "0.0.0.0/0"]
+//}
+//
+//
+//resource "google_compute_firewall" "i14-ni-gcp" {
+//  name = "devsecops-fw-single-ip"
+//  network = "devsecops-network-gcp"
+//
+//  allow {
+//    protocol = "tcp"
+//    ports = [
+//      "135",
+//      "139",
+//      "445",
+//      "593"]
+//  }
+//
+//  description = "INGRESS"
+//  source_ranges = [
+//    "192.168.1.1/32"]
+//}
+//
+//resource "google_compute_firewall" "i23-ni-gcp" {
+//  name = "devsecops-fw-multi-ip"
+//  network = "devsecops-network-gcp"
+//
+//  allow {
+//    protocol = "udp"
+//    ports = [
+//      "137",
+//      "138"]
+//  }
+//
+//  description = "INGRESS"
+//  source_ranges = [
+//    "192.168.1.1/32",
+//    "192.168.1.2/32",
+//    "192.168.1.3/32",
+//    "192.168.1.4/32",
+//    "192.168.1.5/32"]
+//}
+//
+//resource "google_compute_firewall" "i21-ni-gcp" {
+//  name = "devsecops-fw-cidr-ip"
+//  network = "devsecops-network-gcp"
+//
+//  allow {
+//    protocol = "tcp"
+//    ports = [
+//      "22"]
+//  }
+//
+//  description = "INGRESS"
+//  source_ranges = [
+//    "192.168.0.0/16"]
+//}
