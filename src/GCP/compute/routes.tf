d371f1f9e05003c234c2c5c8d7b0a736bcd20623
artsem.ilyayeu <artsem.ilyayeu@algosec.com>
2022-09-18 11:18:56 +0300

    deleted another folder with risks

--- src/GCP/compute/routes.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_route" "foobar" {
-  name        = "devsecops-route"
-  dest_range  = "192.168.0.0/16"
-  network     = "devsecops-network-gcp"
-  next_hop_ip = "192.168.1.1"
-  priority    = 100
-}
